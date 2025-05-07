ffc233e650c0a8fb3ccd08628c967dcf40ed9dbb
Angel Arribasplata <escobar1234123@gmail.com>
2025-05-06 22:21:49 -0500

    Importar código completo de ms-auth

---
+++ ms-auth/src/main/java/com/auth/ms_auth/dto/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.auth.ms_auth.dto;
+
+import lombok.Data;
+
+/**
+ * @file: AutoRequest
+ * @author: Angel Arribasplata
+ * @created: 3/05/2025
+ */
+@Data
+public class AuthRequest {
+    private String username;
+    private String password;
+}
---
+++ ms-auth/src/main/java/com/auth/ms_auth/entity/Role.java
@@ -0,0 +1,12 @@
+package com.auth.ms_auth.entity;
+
+/**
+ * @file: Role
+ * @author: Angel Arribasplata
+ * @created: 3/05/2025
+ */
+public enum Role {
+    USER,
+    ADMIN,
+    SUPERADMIN
+}
